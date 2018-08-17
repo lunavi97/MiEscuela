23e5b2334ac74bcf3b592f91849803d782577b08
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-16 21:50:04 -0300

    Agregados atributos, métodos y propiedades a Estudiante

--- MiEscuela/Estudiante.cs
+++ MiEscuela/Estudiante.cs
@@ -8,5 +8,37 @@ namespace MiEscuela
 {
     class Estudiante
     {
+        #region Atributos
+        private int id;
+        private string matricula;
+        private string nombre;
+        private string gradoGrupo;
+        private bool estatus;
+        #endregion
+
+        #region Propiedades
+        public int Id { get; }
+        public string Matricula { get; }
+        public string Nombre { get; set; }
+        public string GradoGrupo { get; set; }
+        public bool Estatus { get; set; }
+        #endregion
+
+        #region Métodos
+        public string Registrar()
+        {
+            // Registro en la BD
+            string resp = "Estudiante registrado";
+            return resp;
+        }
+
+        public string DarDeBaja()
+        {
+            this.Estatus = false;
+            // Editar el registro en la BD
+            string resp = "Estudiante dado de baja";
+            return resp;
+        }
+        #endregion
     }
 }
