2648cd540629f623b7c6daad57f75835b2d33b46
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-18 22:11:19 -0300

    Modificado método para guardar en BD con parámetro por defecto en falso

--- MiEscuela/Program.cs
+++ MiEscuela/Program.cs
@@ -8,9 +8,19 @@ namespace MiEscuela
 {
     class Program
     {
+        public static void ImprimirMensajeEnPantalla(string mensaje)
+        {
+            Console.WriteLine("Respuesta del sistema: ");
+            Console.WriteLine(mensaje);
+        }
+
         static void Main(string[] args)
         {
             Console.Title = "Sistema de Administración Escolar";
+
+            Profesor profesor = new Profesor();
+            string resp = profesor.Chequear();
+            ImprimirMensajeEnPantalla(resp);
 
             Console.ReadKey();
         }
