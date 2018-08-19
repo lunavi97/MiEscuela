19a8cdae4828ee31d0c0a8158fc6bb21c0aa992b
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-18 23:48:05 -0300

    Definida herencia de clases PorHora y TiempoCompleto con respecto a Profesor

--- MiEscuela/PorHora.cs
+++ MiEscuela/PorHora.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MiEscuela
 {
-    class PorHora
+    class PorHora<Tipo> : Profesor<Tipo>
     {
     }
 }
--- MiEscuela/TiempoCompleto.cs
+++ MiEscuela/TiempoCompleto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MiEscuela
 {
-    class TiempoCompleto
+    class TiempoCompleto<Tipo> : Profesor<Tipo>
     {
     }
 }
